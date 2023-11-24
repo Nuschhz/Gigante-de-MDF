df3455c56c2ff6961cdee9f5108ebaeb00e811e2
Nuschhz <nusch05@hotmail.com>
2023-11-24 16:13:12 -0300

    README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Gigante de MDF
 
 Projeto para a matéria de Programação de Hardware do sexto semestre de Engenharia da Computação
+
+## Softwares
+
+- Arduino IDE
+- SimulIDE
+- Doxygen
--- giganteMDF/giganteMDF.ino
+++ giganteMDF/giganteMDF.ino
@@ -2,23 +2,16 @@
  * @file giganteMDF.ino
  * @brief Gigante-De-MDF Main
  * @author Gustavo Lucio | Gustavo Nusch | Lucas Faria | Rafaela Pinheiro
- * @details https://github.com/Nuschhz/Gigante-de-MDF
+ * @see https://github.com/Nuschhz/Gigante-de-MDF
  * @date 23-11-2023
  */
+#define F_CPU 16000000UL;/**< Definição da CPU para 16MHz.*/
+#include <avr/io.h>/**< Inclusão da biblioteca avr/io.*/
+#include <avr/interrupt.h>/**< Inclusão da biblioteca avr/interrupt.*/
 
-/**
- *@brief definição da CPU e bibliotecas utilizadas.
-*/
-#define F_CPU 16000000UL;
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-/**
- *@brief definição de macros.
-*/
-#define _BV(n) (1<<n)
-#define ALTERNA_BIT(PORT,BIT) PORT ^= (1<<BIT)
-#define LIMPA_BIT(PORT, BIT) PORT &= ~(1<<BIT)
+#define _BV(n) (1<<n)/**< Macro que insere o valor em um determinado BIT.*/
+#define ALTERNA_BIT(PORT,BIT) PORT ^= (1<<BIT)/**< Macro que alterna o valor em um determinado BIT entre 0 e 1.*/
+#define LIMPA_BIT(PORT, BIT) PORT &= ~(1<<BIT)/**< Macro que limpa o determinado BIT.*/
 
 #define LINHA_RETA_AC_1 (!(PINC & _BV(PC4)) && (PINC & _BV(PC2)) && (PINC & _BV(PC3)))
 #define LINHA_RETA_AC_2 (!(PINC & _BV(PC4)) && !(PINC & _BV(PC2)) && !(PINC & _BV(PC3)))
@@ -34,7 +27,10 @@ volatile uint8_t vidasTotais;/**< Váriavel responsável pelas vidas máximas do
 char flag;/**< Variável que controla se o Gigante está funcionando ou não.*/
 
 /**
- *@brief .
+ *@brief Interrupção para cada comparação do Timer1.
+ *
+ *Esta interrupção é chamada sempre que o registrador <b> TCNT1 </b> atinge o registrador de topo <b> OCR1A </b>.
+ *Caso a <b> flag </b> de funcionamento esteja ativa, o laser irá atirar de 1 em 1 segundo.
 */
 ISR(TIMER1_COMPA_vect){
 	if(flag == 1){
@@ -44,48 +40,77 @@ ISR(TIMER1_COMPA_vect){
 		LIMPA_BIT(PORTB, PB4);
 	}
 }
-
 /**
- *@brief definição de macros.
+ *@brief Interrupção para cada conversão ADC.
+ *
+ *Esta interrupção é chamada sempre ocorre uma conversão analógica para digital.
+ *Ela armazena o valor do registro <b> ADC </b> na variável <b> adcValue </b>.
 */
 ISR(ADC_vect){
 	adcValue = ADC;
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que desativa as operações do Gigante de MDF.
+ *
+ *Esta função desativa o robô após ele ter sido atingido 3 vezes.
+ *Desativando todas as variáveis de funcionamento.
+ *@code {.C}
+ * cli();//desabilita as interrupções globais.
+ * adcValue = 0;//zera o valor da variável de conversão analógica.
+ * PORTB |= 0;//zera todos os bits do PORTB.
+ * PORTD |= 0;//zera todos os bits do PORTD.
+ * flag = 0;//zera o valor da variável de funcionamento.
+ *@endcode
 */
-void desliga(){//desliga o robô após ser atingido 3 vezes
-	cli();//desabilita interrupções globais
+void desliga(){
+	cli();
 	adcValue = 0;
-	PORTB |= 0;//desliga as portas no PORTB
-	PORTD |= 0;//desliga as portas no PORTD
-	flag = 0;//desabilita a flag de funcionamento
+	PORTB |= 0;
+	PORTD |= 0;
+	flag = 0;
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que realiza o giro de 180° do Gigante de MDF.
+ *
+ *Esta Função ativa os BITS para sentido anti-horário dos motores.
+ *Também espera o tempo de 5 segundos alterando o modo de funcionamento do <b> Timer1 </b>.
+ * @code {.C}
+ * TCCR1A = 0;//define o Timer1 para modo de operação normal.
+ * TCNT1 = 0;//zera o registro contador.
+ * while(TCNT1 <= 15625);//delay de 1 segundo
+ * //...
+ * TCNT1 = 0;//zera o registro contador.
+ * while(TCNT1 <= 62500);//delay de 4 segundos
+ * //...
+ * @endcode
 */
-void giro(){//faz o robô girar 180 graus ao ser atingido e desativa por 5s antes que possa ser utilizado
-	PORTB &= ~0x06;//desliga as rodas da esquerda
-	PORTD &= ~0x40;//desliga o bit PD6
-	PORTD |= _BV(PD7);//Gira pra direita
+void giro(){
+	PORTB &= ~0x02
+	PORTD &= ~0x40;
+  PORTB |= _BV(PB2);
+	PORTD |= _BV(PD7);
 	TCCR1A = 0;
 	TCNT1 = 0;
-	while(TCNT1 <= 23438);
-	PORTD &= 0x0C;//cancela o giro
-	while(TCNT1 <= 54687);
+	while(TCNT1 <= 15625);
+  LIMPA_BIT(PORTB, PB2);
+  LIMPA_BIT(PORTD, PD7);
+  TCNT1 = 0;
+	while(TCNT1 <= 62500);
 	piscaLaser();
-	sei();//ativa as interrupções globais
+	sei();
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que administra o dano recebido pelo Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * @endcode
 */
-void dano(){//Apaga um ods leds de vida sempre que for acertado no LDR
-	flag = 0;//desabilita a flag de ativação do circuito
-	danoRecebido++;//incrementa a variável de dano recebido
-	char vidasAtuais = vidasTotais - danoRecebido;//cria a variavel local para vidas atuais
+void dano(){
+	flag = 0;
+	danoRecebido++;
+	char vidasAtuais = vidasTotais - danoRecebido;
 	if(vidasAtuais >= 2){
 		ALTERNA_BIT(PORTD,PD4);
 		LIMPA_BIT(PORTB, PB4);
@@ -103,9 +128,14 @@ void dano(){//Apaga um ods leds de vida sempre que for acertado no LDR
     desliga();
 	}
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que faz o Gigante de MDF acelerar.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void acelera(){
 	while(!(PINC & _BV(PC2))){//curva esquerda
@@ -126,39 +156,22 @@ void acelera(){
   LIMPA_BIT(PORTD, PD6);
   LIMPA_BIT(PORTD, PD7);
 }
-
 /**
- *@brief Função que faz o Gigante dar Ré.
- * @code
- * void re(){
- *	while(!(PINC & _BV(PC2))){//curva esquerda
- *	leituraADC();
- *  PORTB |= _BV(PB1);
- *}
- while(!(PINC & _BV(PC3))){//curva direita
- *	leituraADC();
- *  PORTD |= _BV(PD7);
- *}
- while(LINHA_RETA_RE){//linha reta
- *	leituraADC();
- *	PORTB |= _BV(PB1);
- *	PORTD |= _BV(PD7);
- *}
- LIMPA_BIT(PORTB, PB1);
- LIMPA_BIT(PORTB, PB2);
- LIMPA_BIT(PORTD, PD6);
- LIMPA_BIT(PORTD, PD7);
- *}
+ *@brief Função que faz o Gigante de MDF dar ré.
+ *
+ *
+ * @code {.C}
+ * 
  * @endcode
 */
 void re(){
 	while(!(PINC & _BV(PC2))){//curva esquerda
 		leituraADC();
-    PORTB |= _BV(PB1);
+    PORTD |= _BV(PD7);
 	}
 	while(!(PINC & _BV(PC3))){//curva direita
 		leituraADC();
-    PORTD |= _BV(PD7);
+    PORTB |= _BV(PB1);
 	}
 	while(LINHA_RETA_RE){//linha reta
 		leituraADC();
@@ -170,23 +183,33 @@ void re(){
   LIMPA_BIT(PORTD, PD6);
   LIMPA_BIT(PORTD, PD7);
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que inicia o laser do Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void piscaLaser(){
 	TCCR1A = 0;
-	TCCR1A |= _BV(WGM12);//CTC
+	TCCR1A |= _BV(WGM12);
 	TCCR1B = 0;
-	TCCR1B |= _BV(CS10) | _BV(CS12);//prescaler 1024
+	TCCR1B |= _BV(CS10) | _BV(CS12);
 	TIMSK1 = 0;
-	TIMSK1 |= _BV(OCIE1A);//ativa as interrupções do timer1
-	OCR1A = 15625;//valor que sera comparado 
+	TIMSK1 |= _BV(OCIE1A);
+	OCR1A = 15625;
 	TCNT1 = 0;
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void inicializaAnalog(){
 	ADMUX = 0;
@@ -195,18 +218,28 @@ void inicializaAnalog(){
 	ADCSRA = 0;
 	ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS0) | _BV(ADIE);
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void inicializaVidas(){
 	vidasTotais = 3;
 	danoRecebido = 0;
 	PORTD |= _BV(PD2) | _BV(PD3) | _BV(PD4);
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void enableA_PWM(){
 	OCR0B = 191;
@@ -215,9 +248,14 @@ void enableA_PWM(){
 	TCCR0B = 0;
 	TCCR0B |= _BV(CS00);
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void enableB_PWM(){
 	OCR2A = 191;
@@ -226,9 +264,14 @@ void enableB_PWM(){
 	TCCR2B = 0;
 	TCCR2B |= _BV(CS20);
 }
-
 /**
- *@brief definição de macros.
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 void leituraADC(){
 	ADCSRA |= _BV(ADSC);
@@ -240,7 +283,13 @@ void leituraADC(){
 	}
 }
 /**
- *@brief main loop
+ *@brief Função que Gigante de MDF.
+ *
+ *
+ *
+ * @code {.C}
+ * 
+ * @endcode
 */
 int main(void){
 	DDRB |= _BV(PB4);//define o laser atirador como saída
@@ -248,6 +297,7 @@ int main(void){
 	DDRD |= _BV(PD5) | _BV(PD6) | _BV(PD7);//define os bits Pd5~EnB, Pd6-In3, Pd7-In4 como saída
 	DDRD |= _BV(PD2) | _BV(PD3) | _BV(PD4);//define os Leds de vida como saída
 	DDRC = 0x00;//define os bits Pc0, Pc2-esquerda, Pc3-direita, Pc4-acelera, Pc5-ré como entrada
+  DDRC |= _BV(PC6);
 	PORTC |= _BV(PC2) | _BV(PC3)| _BV(PC4) | _BV(PC5);//ativa os resistores de pull-up do PORTC
 	flag = 1;
 
